0bdfa62ebf95adcfb3d61fe3ff2a7585a1cadb08
Reza <shariatireza25@gmail.com>
2024-08-05 03:38:15 +0330

    full object orinted

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
 "use strict";
-let make_hello = (a, b) => {
-    return a + b;
-};
-let s2 = 13;
-let s3 = "alireza";
-let obj = {
-    name: "lambo",
-    last_name: "ginni",
-    price: 199,
-    get_price: function () {
-        return this.price;
-    },
-    drivers: ["ali", "reza", "farzan"]
-};
-class basic_printer {
-    constructor() {
-        this.name1 = "ss";
-        this.pelak1 = "sasd";
+class country {
+    constructor(name1, vahed) {
+        this.name = name1;
+        this.vahed_pool = vahed;
     }
-    seda() {
-        console.log("aleasd");
+    get_name() {
+        return this.name;
+    }
+    get_pool() {
+        return this.vahed_pool;
     }
 }
-console.log(basic_printer.name);
-//console.log(obj);
-//console.log(obj.get_price());
+let iran = new country("iran", "rial");
+console.log(iran);
--- index.ts
+++ index.ts
@@ -1,55 +1,22 @@
-let make_hello = (a:string , b:number):string=>{
-    return a+b;
-}
 
-let s2:number = 13;
-let s3:string = "alireza";
-type Car={
-    name:string
-    last_name:string
-    price:number
-    get_price: (paraama:void)=>number
-    drivers:string[]
-}
-
-let obj:Car={
-    name:"lambo",
-    last_name:"ginni",
-    price:199,
-    get_price:function():number{
-        return this.price
-    },
-    drivers:["ali" , "reza"  ,"farzan"]
-}
-
-
-interface motor{
-    name1:string
-    pelak1:string
-    seda1:()=>void
+//object orinted
+class country{
+    private name : string
+    private vahed_pool : string
+        constructor(name1:string , vahed:string){
+            this.name = name1;
+            this.vahed_pool = vahed;
+        }
+        get_name():string{
+            return this.name;
+        }
+        get_pool():string{
+            return this.vahed_pool;
+        }
 
 }
 
-let s:motor={
-    name1 : "asd",
-    pelak1 : "asdasd",
-    seda1 : function(){
-        console.log("asdas");
-    }
-}
-
-
-interface person{
-
-    make_voice : ()=>void
-
-}
-
-class myclass implements person{
-    make_voice(){
-        console.log("");
-    }
-}
-
-
+let iran = new country("iran" , "rial");
+console.log(iran);
+//// generic type
 
